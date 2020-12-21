e1dc0e89aeec3f854895a4dcc84f76f371584018
raeleus <raymond.ray3k@gmail.com>
2020-12-21 06:35:14 -0800

    Added filePathNoExtension() to utils. Add to template

--- core/src/com/ray3k/template/Utils.java
+++ core/src/com/ray3k/template/Utils.java
@@ -32,6 +32,7 @@ public class Utils {
     private static final Ray rayTemp = new Ray();
     private static final Vector2 vector2 = new Vector2();
     private static Pattern fileNamePattern = new Pattern("([^/.]+)(?:\\.?[^/.])*$");
+    private static Pattern noExtensionPattern = new Pattern(".*(?=\\.)");
     private static final Rectangle tempRectangle1 = new Rectangle();
     
     public static Array<Actor> getActorsRecursive(Actor actor) {
@@ -141,6 +142,12 @@ public class Utils {
         Matcher matcher = fileNamePattern.matcher(path);
         matcher.find();
         return matcher.group(1);
+    }
+    
+    public static String filePathNoExtension(String path) {
+        Matcher matcher = noExtensionPattern.matcher(path);
+        matcher.find();
+        return matcher.group(0);
     }
     
     public static String mouseButtonToString(int button) {
